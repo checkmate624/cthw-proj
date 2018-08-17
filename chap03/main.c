cde944b31257f7264bf55293d073dff00d57b384
Levi Jones <leviwill.j@gmail.com>
2018-08-17 18:07:03 -0400

    Added a comment

--- chap03/main.c
+++ chap03/main.c
@@ -2,6 +2,7 @@
 
 int main()
 {	
+	//Output code that will write 'Hello 2018'
 	printf("#include <stdio.h>\n\nint year = 2018;\n\nint main()\n{\n	printf(\"Hello, %%d!\\n\", year);\n	return 0;\n}\n");
 
 	return 0;
