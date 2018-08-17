ebd3e291237ed68041b7dbf5273fa0fe301c0424
Levi Jones <leviwill.j@gmail.com>
2018-08-17 14:19:58 -0400

    Finished the chapter challenge.

--- chap03/main.c
+++ chap03/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main()
-{
-	int x = 10;
-	printf("some stuff %d\n", x);
+{	
+	printf("#include <stdio.h>\n\nint year = 2018;\n\nint main()\n{\n	printf(\"Hello, %%d!\\n\", year);\n	return 0;\n}\n");
+
 	return 0;
 }
