001e90910f35faf2541cf0dd7724318057920a76
Levi Jones <leviwill.j@gmail.com>
2018-10-04 14:34:49 -0400

    Finished challenge for chapter 08

---
+++ chap08/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	//Generate random seed
+	srand(time(0));
+	
+	//Define array of numbers spelled out in english
+	char *words[] = {
+		"One", "Two", "Three", "Four", "Five", "Six", "Seven",
+		"Eight", "Nine", "Ten"
+	};
+	
+	//Generate random number
+	int randomNumber = rand();
+
+	//Get last digit of random number
+	int lastDigitOfRandom = randomNumber % 10;
+
+	//Print random item of the *words array
+	printf("%s\n", words[lastDigitOfRandom]);
+
+	return 0;
+}
