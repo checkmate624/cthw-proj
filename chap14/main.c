f29ef426e98981665316b64e919c8bee586daeba
Levi Jones <leviwill.j@gmail.com>
2018-11-20 14:40:44 -0500

    Finished the chapter 14 challenge.

---
+++ chap14/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int steps = 0;
+double value = 0;
+double precision = .001;
+
+double generate_number()
+{
+	return rand() % 1000;
+}
+
+double find_square_root(double num, double precision)
+{
+	//If the function received valid arguments
+	if(num > 0 && precision > 0)
+	{ 
+		printf("Your starting number is %f\n", num);
+		value = num / 2;
+
+		//While the difference is larger than the allotted value
+		while(abs((value*value) - num) > precision)
+		{
+			value = (value + (num / value)) / 2;
+			steps++;
+		}
+
+		printf("It took %i steps to find\n", steps);
+		return value;
+	}
+	else
+	{
+		printf("Pass better arguments\n");
+		return 0;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	srand(time(0));
+
+	printf("The square root is %f\n", find_square_root(generate_number(), precision));
+
+	return 0;
+}
